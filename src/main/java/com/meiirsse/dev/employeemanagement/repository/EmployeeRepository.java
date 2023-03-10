6b8a1598b2701958e7f3b745b5a2e43dfa80e324
Meiirlan Serikbay <114663479+meiirsse@users.noreply.github.com>
2023-03-10 10:21:02 +0600

    Initial commit

---
+++ src/main/java/com/meiirsse/dev/employeemanagement/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.meiirsse.dev.employeemanagement.repository;
+
+import com.meiirsse.dev.employeemanagement.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
---
+++ src/main/java/com/meiirsse/dev/employeemanagement/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.meiirsse.dev.employeemanagement.service;
+
+import com.meiirsse.dev.employeemanagement.entity.EmployeeEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeService {
+    List<EmployeeEntity> findAllEmployee();
+    Optional<EmployeeEntity> findById(Long id);
+    EmployeeEntity saveEmployee(EmployeeEntity employeeEntity);
+    EmployeeEntity updateEmployee(EmployeeEntity employeeEntity);
+    void deleteEmployee(Long id);
+}
